438819c9e8ea399504c77561d3a66a4c61818f58
Pushpraj pandey <chunmunpandey444@gmail.com>
2026-07-05 21:27:04 +0530

    Update script.js

--- script.js
+++ script.js
@@ -1,6 +1,40 @@
 function addTask() {
     let taskInput = document.getElementById('taskInput');
-    let taskText = taskInput.value.trim();
+    let taskText = taskInput.value.trim()function updateClock() {
+    let now = new Date();
+    
+    let hours = now.getHours();
+    let minutes = now.getMinutes();
+    let seconds = now.getSeconds();
+    let ampm = hours >= 12 ? 'PM' : 'AM';
+
+    
+    hours = hours % 12;
+    hours = hours ? hours : 12; 
+
+
+    hours = hours < 10 ? '0' + hours : hours;
+    minutes = minutes < 10 ? '0' + minutes : minutes;
+    seconds = seconds < 10 ? '0' + seconds : seconds;
+
+    document.getElementById('clock').innerHTML = `${hours}:${minutes}:${seconds} ${ampm}`;
+
+    let realHour = now.getHours(); 
+    let greetingText = document.getElementById('greeting');
+
+    if (realHour >= 6 && realHour < 18) {
+        document.body.classList.add('day-mode');
+        greetingText.innerHTML = "☀️ Good Morning, Pushpraj";
+    } else {
+        document.body.classList.remove('day-mode');
+        greetingText.innerHTML = "🌙 Good Night, Pushpraj";
+    }
+}
+
+
+setInterval(updateClock, 1000);
+
+updateClock();;
 
     if (taskText === "") {
         alert("Please enter a valid task!");
@@ -17,7 +51,7 @@ function addTask() {
     li.style.marginBottom = "8px";
     li.style.backgroundColor = "#2a2a2a";
     li.style.borderRadius = "6px";
-    li.style.borderLeft = "4px solid #00adb5"; // Neon accent border
+    li.style.borderLeft = "4px solid #00adb5"; 
 
     li.innerHTML = `
         <span style="flex: 1; text-align: left; cursor: pointer;" onclick="toggleComplete(this)">${taskText}</span>
@@ -27,24 +61,22 @@ function addTask() {
     
     taskList.appendChild(li);
 
-    // 7. Task add hone ke baad input box ko wapas khali kar dena
+
     taskInput.value = "";
 }
 
-// 2. Task ko delete karne ka function
 function deleteTask(button) {
-    // Button ke parent element (li) ko dhoodh kar use list se remove karna
+    
     let li = button.parentElement;
     li.remove();
 }
 
-// 3. Task complete hone par strike-through (cut line) lagane ka function
 function toggleComplete(taskSpan) {
     if (taskSpan.style.textDecoration === "line-through") {
         taskSpan.style.textDecoration = "none";
         taskSpan.style.opacity = "1";
     } else {
         taskSpan.style.textDecoration = "line-through";
-        taskSpan.style.opacity = "0.5"; // Task halka dhundhla ho jayega
+        taskSpan.style.opacity = "0.5"; 
     }
 }
